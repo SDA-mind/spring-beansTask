7cf39cf93ce250edd33d873808a53a03ddc4ec30
SDA-mind <dima-sherbakov@mail.ru>
2020-08-17 10:12:28 +0300

    First

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,21 @@
 package app.config;
 
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+    @Bean(name="Dog")
+    public Dog getDog() {
+        return new Dog();
+    }
+
+    @Bean(name="Timer")
+    public Timer getTimer() {
+        return new Timer();
+    }
 }
